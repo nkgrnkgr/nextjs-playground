71f92e53f18e5f6ce0c7b600cf7661077638365b
nkgrnkgr <beefupme@gmail.com>
2022-07-12 20:16:31 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,17 +14,14 @@ export default function Index({ users }: { users: User[] }) {
 
   return (
     <div>
-      <p>
-        To test the CORS route, open the console in a new tab on a different
-        domain and make a POST / GET / OPTIONS request to <b>/api/cors</b>.
-        Using a different method from those mentioned will be blocked by CORS
-      </p>
+      <p>This is pre-fetched Props</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.id}</li>
         ))}
       </ul>
       <hr />
+      <p>This is fetched Props</p>
       <ul>
         {fetchedUsers.map((user) => (
           <li key={user.id}>{user.id}</li>
